e89220e85f628991d0b4eca0a26e0e783859e06b
Arman Gurkan <arman@secelektrik.com>
2020-01-29 14:27:10 -0800

    hey

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
-docker build -t armangurkan1/multi-client:latest -t armangurkan1/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t armangurkan1/multi-server:latest -t armangurkan1/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t armangurkan1/multi-worker:latest -t armangurkan1/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker build -t armangurkan1/multi-client:latest -t ./client/Dockerfile ./client
+docker build -t armangurkan1/multi-server:latest -t ./server/Dockerfile ./server
+docker build -t armangurkan1/multi-worker:latest -t ./worker/Dockerfile ./worker
 docker push armangurkan1/multi-client:latest
 docker push armangurkan1/multi-server:latest
 docker push armangurkan1/multi-worker:latest
@@ -8,6 +8,6 @@ docker push armangurkan1/multi-client:$SHA
 docker push armangurkan1/multi-server:$SHA
 docker push armangurkan1/multi-worker:$SHA
 kubectl apply -f ./k8s
-kubectl set image deployments/server-deployment server=armangurkan1/multi-server:$SHA
-kubectl set image deployments/client-deployment client=armangurkan1/multi-client:$SHA
-kubectl set image deployments/worker-deployment worker=armangurkan1/multi-worker:$SHA
+kubectl set image deployments/server-deployment server=armangurkan1/multi-server:latest
+kubectl set image deployments/client-deployment client=armangurkan1/multi-client:latest
+kubectl set image deployments/worker-deployment worker=armangurkan1/multi-worker:latest
