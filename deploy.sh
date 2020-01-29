2fa1a71e6f51bb7f507e9d38f9e08353f533b4a1
Arman Gurkan <arman@secelektrik.com>
2020-01-28 19:00:17 -0800

    change

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,11 @@
-docker build -t armangurkan1/multi-client:latest -t armangurkan1/multi-client:"$SHA" -f ./client/Dockerfile ./client
-docker build -t armangurkan1/multi-server:latest -t armangurkan1/multi-server:"$SHA" -f ./server/Dockerfile ./server
-docker build -t armangurkan1/multi-worker:latest -t armangurkan1/multi-worker:"$SHA" -f ./worker/Dockerfile ./worker
+docker build -t armangurkan1/multi-client:latest -f ./client/Dockerfile ./client
+docker build -t armangurkan1/multi-server:latest -f ./server/Dockerfile ./server
+docker build -t armangurkan1/multi-worker:latest -f ./worker/Dockerfile ./worker
 docker push armangurkan1/multi-client:latest
 docker push armangurkan1/multi-server:latest
 docker push armangurkan1/multi-worker:latest
-docker push armangurkan1/multi-client:"$SHA"
-docker push armangurkan1/multi-server:"$SHA"
-docker push armangurkan1/multi-worker:"$SHA"
+
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=armangurkan1/multi-server:"$SHA"
-kubectl set image deployments/client-deployment client=armangurkan1/multi-client:"$SHA"
-kubectl set image deployments/worker-deployment worker=armangurkan1/multi-worker:"$SHA"
+kubectl set image deployments/server-deployment server=armangurkan1/multi-server
+kubectl set image deployments/client-deployment client=armangurkan1/multi-client
+kubectl set image deployments/worker-deployment worker=armangurkan1/multi-worker
